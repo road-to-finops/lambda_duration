de7941b510cfddb9e59a45af6a7d12577a7fe74f
Stephanie Gooch <stephanie.gooch@hotmail.com>
2020-08-31 16:42:10 +0100

    added code

---
+++ Readme.md
@@ -0,0 +1,17 @@
+## Lambda functions running for too long with far too much memory
+
+
+### Description:
+Prints list with the minimum, average and maximum number of invocations and duration of each lambda function in the account as well as the memory attached to the lambda.
+
+This exports a easy read txt file as well as a json file which can be read by athena
+
+### Requirments
+Access keys setup to account you wish to access lamda data
+Python 3
+Boto3 
+AWS cli
+
+### How to run
+
+``` python3 main.py   ```
---
+++ main.py
@@ -0,0 +1,133 @@
+import time
+import datetime
+import boto3
+import json
+import sys
+import logging
+# initiate logging
+logger = logging.getLogger()
+
+def main():
+    output=open("LAMBDA_average_usage.txt", "w")
+    functions_list = ls_fun_pag()
+    
+    data_list = []
+
+    '''
+    functions_list=[]
+    ls = ls_func()
+    for f_name in ls.get('Functions'):
+        functions_list.append(f_name.get('FunctionName'))
+    '''
+    for name in functions_list:
+        metric_data = get_metrics_lambda(name,output)
+        if metric_data != None: 
+            data_list.append(metric_data)
+    output.close()
+    make_json(data_list)
+
+def ls_fun_pag():
+    client = boto3.client('lambda')
+    function_list = []
+
+    paginator = client.get_paginator('list_functions')
+    response_iterator = paginator.paginate()
+
+    for page in response_iterator:
+        f = page['Functions']
+        for function in f:
+            function_list.append(function['FunctionName'])
+    
+    return function_list
+
+
+def ls_func():
+    client = boto3.client('lambda')
+    response = client.list_functions()
+    return(response)
+
+def get_memory(fName):
+    client = boto3.client('lambda')
+    response = client.get_function(
+         FunctionName=fName)
+    MemorySize = response['Configuration']['MemorySize']
+    return MemorySize
+
+def make_json(records):
+    logger.info("Creating json file")    
+    
+    try:
+        with open(f"lambda.json", "w") as outfile:
+            for result in records:
+                json.dump(result, outfile)
+                outfile.write('\n')
+        logger.info('json created')
+
+    except:
+        logging.exception("!!!json creation failed!!!")
+        raise
+
+def get_metrics_lambda(fName,output):
+    MemorySize = get_memory(fName)
+    start_time = datetime.datetime.utcnow() - datetime.timedelta(weeks=2)
+    end_time = datetime.datetime.utcnow()
+    client = boto3.client('cloudwatch')
+    response_invocation = client.get_metric_statistics(
+        Namespace='AWS/Lambda',
+        MetricName='Invocations',
+        Dimensions=[
+            {
+                'Name': 'FunctionName',
+                'Value': fName
+            }
+        ],
+        StartTime=start_time,
+        EndTime=end_time,
+        Period=1209600,
+        Statistics=[
+        'Minimum','Average','Maximum'
+        ],
+        Unit='Count'
+     )
+    for data in response_invocation.get('Datapoints'):
+        min = data.get('Minimum')
+        av = data.get('Average')
+        max = data.get('Maximum')
+        #output.write("Function name:" + " " + fName + "\n" + "\n" + "Invocations:" + "\n" + "Minimum:" + str(min) + "\n" + "Average:" + str(av) + "\n" + "Maximum:" + str(max) + "\n")
+
+    response_duration = client.get_metric_statistics(
+        Namespace='AWS/Lambda',
+        MetricName='Duration',
+        Dimensions=[
+            {
+                'Name': 'FunctionName',
+                'Value': fName
+            }
+        ],
+        StartTime=start_time,
+        EndTime=end_time,
+        Period=1209600,
+        Statistics=[
+        'Minimum','Average','Maximum'
+        ],
+        Unit='Milliseconds'
+     )
+    
+    for data in response_duration.get('Datapoints'):
+        min = data.get('Minimum')
+        av = data.get('Average')
+        max = data.get('Maximum')
+
+        json_data = {"FucntionName": fName, "Minimum": str(min), "Average": str(av), "Maximum": str(max), "MemorySize": str(MemorySize)}
+        print(json_data)
+        return json_data
+
+        #print ("Duration:" + "\n" + "Minimum:" + str(min) + "\n" + "Average:" + str(av) + "\n" + "Maximum:" + str(max) + "\n" + 'MemorySize:'+ str(MemorySize))
+        #output.write("Duration:" + "\n" + "Minimum:" + str(min) + "\n" + "Average:" + str(av) + "\n" + "Maximum:" + str(max)+ "\n" + 'MemorySize:'+ str(MemorySize))
+    #print ("******************************************************************************************************************")
+    #output.write("\n******************************************************************************************************************\n")
+
+
+
+if __name__ == '__main__':
+    main()
