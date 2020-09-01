821e08701628778430919863ede6559738e49b58
sgooch <stephanie.gooch@kpmg.co.uk>
2020-09-01 13:27:25 +0100

    most recent log stream

--- get_logs.py
+++ get_logs.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger()
 
 
-def check_log_group(logGroupNamePrefix):
+def check_log_group(client, logGroupNamePrefix):
     client = boto3.client('logs')
     try:
         response = client.describe_log_groups(
@@ -20,10 +20,25 @@ def check_log_group(logGroupNamePrefix):
                 logGroupName = function['logGroupName']
                 if logGroupName == logGroupNamePrefix:
                     print(function['arn'])
+                    return function['arn']
         else:
             print(f"{logGroupNamePrefix} is not a log group")
+            return None
     except Exception as e:
         print(e)
 
+        
+def get_log_stream(client, logGroupNamePrefix):
+    response = client.describe_log_streams(
+    logGroupName=logGroupNamePrefix,
+    orderBy='LastEventTime',
+    descending=True
+    )
+    
+    print(response['logStreams'][0]['logStreamName'])
+
+client = boto3.client('logs')
 logGroupNamePrefix = '/aws/lambda/service_dynamo'
-check_log_group(logGroupNamePrefix)
+check = check_log_group(client, logGroupNamePrefix)
+if check != None:
+    get_log_stream(client, logGroupNamePrefix)
